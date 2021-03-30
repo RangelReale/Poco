d83e70def0fb159ae570fcd76126fa68ff3703de
Rangel Reale <rangelspam@gmail.com>
2021-03-30 14:38:44 -0300

    add missing break

--- Net/src/HTTPClientSession.cpp
+++ Net/src/HTTPClientSession.cpp
@@ -464,6 +464,7 @@ void HTTPClientSession::proxyAuthenticateImpl(HTTPRequest& request, const ProxyC
 			_proxyDigestCreds.setPassword(proxyConfig.password);
 			proxyAuthenticateDigest(request);
 		}
+		break;
 
 	case PROXY_AUTH_NTLM:
 		if (_ntlmProxyAuthenticated)
